d8c3c56048fc064d1f748c243cde26a1956f7a22
Vaibhav Gupta <vibhigupta@protonmail.com>
2020-08-30 12:36:39 +0530

    Guessing game in Bash

---
+++ guessinggame.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#File: guessinggame.sh
+
+echo " Guess the number of file in this directory "
+read guess
+
+function try {
+	echo "Please! Try again"
+	read guess
+}
+
+tdir=$(ls  | wc -l )
+
+
+while [[ $guess != $tdir ]]
+    do
+
+        if [[ $guess -gt $tdir ]]
+            then 
+                echo " Your guessed number is too High "
+        else 
+            echo " Your guessed number is too Low"
+        fi    
+    try
+done
+
+echo "You guessed it correctly congo!!"
+
