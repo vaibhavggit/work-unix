2b62db55ba212fab1188db15041822a926d791f8
Vaibhav Gupta <vibhigupta@protonmail.com>
2020-08-30 13:55:56 +0530

    Adding regex changes which werent added in older commit

--- guessinggame.sh
+++ guessinggame.sh
@@ -12,17 +12,26 @@ function try {
 tdir=$(ls -p | grep -v / | wc -l)
 
 
-while [[ $guess != $tdir ]]
-    do
-
+while [ -v $comp] 
+do
+   if [[ $guess =~ ^[0-9]+$ ]]
+   then
         if [[ $guess -gt $tdir ]]
             then 
                 echo " Your guessed number is too High "
-        else 
-            echo " Your guessed number is too Low"
-        fi    
-    try
+                try     
+elif [[ $guess -lt $tdir ]]
+            then
+                echo " Your guessed number is too Low"
+                try
+        elif [[ $guess == $tdir ]]
+            then 
+                echo "You guessed it correctly congo!!"
+                comp=true
+        fi
+     else 
+        echo "Please enter a integer"
+        try
+    fi
 done
-
-echo "You guessed it correctly congo!!"
 
